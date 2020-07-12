b9f2d5ec735618d9b8bf4e4c0cf0ff89df4cea0b
Md. Abu Taher <entrptaher@gmail.com>
2020-07-12 07:26:44 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/public/'
   },
   plugins:  debug ?  [
       new ExtractTextPlugin("styles.css"),
